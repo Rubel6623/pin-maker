ae78cbb9bcfcd3dfe440cad281db621a895a978d
Rubel Rudra <rubelrudra27@gmail.com>
2024-05-11 01:10:14 +0600

    generate random pin with random function & display pin

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
+function getPin(){
+  const pin=generatePin();
+  const pinStr=pin+'';
+  if(pinStr.length===4){
+    return pin;
+  }
+  else{
+    return getPin();
+  }
+}
+
 function generatePin(){
   const randomPin= Math.round(Math.random()*10000);
   return  randomPin;
 }
 
-console.log(generatePin())
+document.getElementById('pin-generator').addEventListener('click',function(){
+  const pin=getPin();
+  document.getElementById('pin-field').value=pin;
+})
+console.log(generatePin()) 
